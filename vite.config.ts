9c3756f5a8218aaaadd77a745f65e72910718fd1
aayman_97 <ahmedayman.ayman80@gmail.com>
2023-04-01 17:10:22 +0200

    Replace dynamic require with dynamic import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-const path = require("path");
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
