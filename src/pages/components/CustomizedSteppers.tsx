4eaa97fdb8a77888d95c8926bbac36e8e9c1a9cb
hazemayman25 <hazemhamed98@gmail.com>
2023-04-02 23:46:42 +0200

    Ignore unnecessary Typescript compilation errors

--- src/pages/components/CustomizedSteppers.tsx
+++ src/pages/components/CustomizedSteppers.tsx
@@ -14,6 +14,7 @@ import "../../assets/images/checkmark-outline.svg";
 import { ShipmentState, ShipmentTracking, TransitEventState } from "@/types.js";
 
 const ColorlibConnector = styled(StepConnector)(
+  // @ts-ignore
   ({ theme, color, vertical }) => ({
     [`&.${stepConnectorClasses.alternativeLabel}`]: {
       top: 22,
@@ -180,6 +181,8 @@ export default function CustomizedSteppers({
         activeStep={activeStep}
         orientation={isVertical ? "vertical" : "horizontal"}
         connector={
+          // @ts-ignore
+
           <ColorlibConnector color={statusColor} vertical={isVertical} />
         }
       >
@@ -191,7 +194,11 @@ export default function CustomizedSteppers({
               )}
             >
               <h4 style={{ fontFamily: "Cairo-Bold" }}>
-                {ShipmentState[label.name]}
+                {
+                  // @ts-ignore
+
+                  ShipmentState[label.name]
+                }
               </h4>
             </StepLabel>
           </Step>
